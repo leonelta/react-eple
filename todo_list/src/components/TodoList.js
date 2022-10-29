a566e3803c6de636ef2dfa44cbdbe5c7199c945b
leonelta <ketchakaren@gmail.com>
2022-10-29 21:16:18 +0200

    dec;laring the state

---
+++ todo_list/src/components/TodoList.js
@@ -0,0 +1,11 @@
+import React, {useState} from 'react';
+import TodoForm from './TodoForm';
+
+function TodoList() {
+  const [todos, setTodos] = useState([])
+  return (
+    <div>TodoList</div>
+  )
+}
+
+export default TodoList
