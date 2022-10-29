b1ca3dc73fa7967a27f3ddb1db40eee26d578797
leonelta <ketchakaren@gmail.com>
2022-10-29 21:23:47 +0200

    importing TodoList.js in App.js

--- todo_list/src/components/TodoList.js
+++ todo_list/src/components/TodoList.js
@@ -4,7 +4,10 @@ import TodoForm from './TodoForm';
 function TodoList() {
   const [todos, setTodos] = useState([])
   return (
-    <div>TodoList</div>
+    <div>
+      <h1>What's the plan for today</h1>
+      <TodoForm />
+    </div>
   )
 }
 
